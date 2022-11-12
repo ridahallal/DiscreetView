3c8ac2b9c228f6d97979ac9627f7a76344f44285
Rida Hallal <rida.hallal@gmail.com>
2022-11-12 17:37:49 +0100

    chore: cleanup

--- Demo/Pods/Headers/Public/DiscreetView/DiscreetView-umbrella.h
+++
@@ -1,1 +0,0 @@
-../../../Target Support Files/DiscreetView/DiscreetView-umbrella.h
--- Demo/Pods/Target Support Files/DiscreetView/DiscreetView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DiscreetView : NSObject
-@end
-@implementation PodsDummy_DiscreetView
-@end
